41071288bc7b5666975716359e3a331d8a1e3cca
Nestor Vera <nestorvera@me.com>
2017-10-11 13:17:09 +0200

    :wrench: Enable freeMode and mousewheel support in features swiper

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
-  // spaceBetween: 30,
   centeredSlides: true,
+  freeMode: true,
+  freeModeSticky: true,
+  mousewheel: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
