d471ecfd2c7a17aa2d0950fe57029f8b72e189c6
Nestor Vera <nestorvera@me.com>
2017-10-11 12:44:03 +0200

    :sparkles: Add pagination to features swiper

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,4 +2,8 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   // spaceBetween: 30,
   centeredSlides: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
