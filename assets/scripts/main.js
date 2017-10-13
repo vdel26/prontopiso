13e6cf386ece8ab5b2c87081d2a4fbbecb3d779a
Nestor Vera <nestorvera@me.com>
2017-10-13 13:05:06 +0200

    :ok_hand: Disable sticky mode for swiper

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,8 +2,12 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   centeredSlides: true,
   freeMode: true,
-  freeModeSticky: true,
-  mousewheel: true,
+  // freeModeSticky: true,
+  mousewheel: {
+    enbled: true,
+    invert: true,
+    forceToAxis: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
