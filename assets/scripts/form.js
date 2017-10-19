d1f9ae4d0039d7749f325d38be348444a708ba0c
Nestor Vera <nestorvera@me.com>
2017-10-19 18:09:32 +0200

    :construction: Validate form inputs and save results in response object

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -1,51 +1,57 @@
-var google_address;
-var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
-    locality: 'long_name',
-    administrative_area_level_2: 'long_name',
-    country: 'long_name',
-    postal_code: 'short_name'
-};
-
-var prontopiso_address_model = {
-    postal_code:'zipCode',
-    street_number:'streetNumber',
-    route:'street',
-    locality:'city',
-    administrative_area_level_2:'province',
-    country:'country'
-};
+var google_address
+  , componentForm = {
+      street_number: 'short_name',
+      route: 'long_name',
+      locality: 'long_name',
+      administrative_area_level_2: 'long_name',
+      country: 'long_name',
+      postal_code: 'short_name'
+    }
+  , prontopiso_address_model = {
+      postal_code:'zipCode',
+      street_number:'streetNumber',
+      route:'street',
+      locality:'city',
+      administrative_area_level_2:'province',
+      country:'country'
+    };
 
 function initAutocomplete() {
-    // Create the google_address object
-    google_address = new google.maps.places.Autocomplete(
-        /** @type {!HTMLInputElement} */(document.getElementById('address-street')),
-        {types: ['geocode']});
-
-    // When the user selects an address from the dropdown, populate the address
-    // fields in the form.
-    google_address.addListener('place_changed', fillInAddress);
+  // Create the google_address object
+  google_address = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */
+    document.getElementById('address-street'), {
+      types: ['geocode'],
+      componentRestrictions: { country: 'es' }
+    }
+  );
+  // When the user selects an address from the dropdown, populate the address fields in the form.
+  google_address.addListener('place_changed', fillInAddress);
 }
 
 function fillInAddress() {
-    // Get the place details from the google_address object.
-    var place = google_address.getPlace();
-    var prontopiso_address = {};
-    // Get each component of the address from the place details
-    // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-            var val = place.address_components[i][componentForm[addressType]];
-            prontopiso_address[prontopiso_address_model[addressType]] = val;
-        }
+  // Get the place details from the google_address object.
+  var place = google_address.getPlace()
+    , prontopiso_address = {};
+  // Get each component of the address from the place details and fill the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      prontopiso_address[prontopiso_address_model[addressType]] = val;
     }
-    console.log(prontopiso_address);
+  }
+  response.address = prontopiso_address;
 }
 
 
-var fieldsets = document.querySelectorAll('fieldset');
+
+var forms = document.querySelectorAll('form')
+  , fieldsets = document.querySelectorAll('fieldset')
+  , inputs = document.querySelectorAll('input')
+  , response = {};
+
+
 document.addEventListener('wheel', function() {
   // Get closest element to window center
   var elem = document.elementFromPoint( window.innerWidth / 2, window.innerHeight / 2 );
@@ -55,7 +61,7 @@ document.addEventListener('wheel', function() {
       fieldsets[i].style.opacity = '';
     } elem.style.opacity = 1;
     // Focus first input element in current fieldset
-    elem.querySelectorAll('input')[0].focus();
+    // elem.querySelectorAll('input')[0].focus();
   }
 }, supportsPassive ? { passive: true } : false);
 
@@ -64,30 +70,46 @@ document.addEventListener('wheel', function() {
 /* Form Validation */
 
 // Add the novalidate attribute when the JS loads
-var forms = document.querySelectorAll('form');
 for (var i = 0; i < forms.length; i++) {
+
+  // Disable HTML validation if JavaScript is running
   forms[i].setAttribute('novalidate', true);
+
+  // Before submitting the form…
+  forms[i].addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log( response );
+  }, false);
 }
 
-var inputs = document.querySelectorAll('input');
-for (i = 0; i < fieldsets.length; ++i) {
+// Attach blur event to inputs
+for (i = 0; i < inputs.length; ++i) {
 
-  // Attach blur event to inputs
   inputs[i].addEventListener('blur', function(e) {
-    var validity = this.validity;
-    if( validity.valid ) {
-      console.log( 'OK' );
-    } else {
-      console.log( 'NOT OK' );
-    }
-  }, supportsPassive ? { passive: true } : false);
+    var validity = this.validity
+      , error = document.querySelector('#' + this.name + '-error');
+    if (this.value && validity.valid && error) {
+      error.classList.remove('db');
+    } else if (error) error.classList.add('db');
 
-  // Attach input event to inputs
-  inputs[i].addEventListener('input', function(e) {
-    var validity = this.validity;
-    if( validity.valid ) {
-      // console.log( this );
-    }
-  }, supportsPassive ? { passive: true } : false);
+    // Save values in response object
+    if (this.value && this.name !== 'cancel' && this.name !== 'submit') {
+      var responseAddy = this.name.split('-')
+        , value = this.value;
 
+      // Make sure the value is the right type
+      if (value === 'true') value = true;
+      else if (value === 'false') value = false;
+      else if (this.type === 'radio' || this.type === 'number') value = parseInt(value, 10);
+      else if (this.type === 'date') value = value + 'T00:00:00+00:00';
+
+      // Save the value where it belongs to
+      if (responseAddy.length === 1) {
+        response[responseAddy] = value;
+      } else {
+        if (!response[responseAddy[0]]) response[responseAddy[0]] = {};
+        response[responseAddy[0]][responseAddy[1]] = value;
+      }
+    }
+  });
 }
