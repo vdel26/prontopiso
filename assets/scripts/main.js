9645add15a2fb1870f8f3fb9363f98ab874e2d66
Nestor Vera <nestorvera@me.com>
2017-11-09 16:54:40 +0100

    :art: Minor formatting change

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,7 +6,7 @@ try {
   var opts = Object.defineProperty({}, 'passive', {
     get: function() { supportsPassive = true; }
   });
-  window.addEventListener("test", null, opts);
+  window.addEventListener('test', null, opts);
 } catch (e) {}
 
 
